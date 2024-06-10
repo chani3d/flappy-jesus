67a938f84d6ab5be33c9b41ee846f159ca3f25bb
Sebastian Cruz Go <sebastia.cruzgo@hes-so.ch>
2024-06-10 08:38:25 +0200

    working on keyboard manager problem

--- src/FlappyJesus.scala
+++ src/FlappyJesus.scala
@@ -5,9 +5,11 @@ import objects._
 import ch.hevs.gdx2d.desktop.PortableApplication
 import ch.hevs.gdx2d.lib.GdxGraphics
 import ch.hevs.gdx2d.lib.interfaces.DrawableObject
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.math.{Interpolation, Vector2}
 
 import java.util
+import scala.collection.mutable
 
 object FlappyJesus extends App {
   new FlappyJesus
@@ -15,17 +17,35 @@ object FlappyJesus extends App {
 
 class FlappyJesus extends PortableApplication(1920, 1080) {
   private var toDraw: util.Vector[DrawableObject] = new util.Vector[DrawableObject]
+  // Key management
+  private val keyStatus: mutable.HashMap[Int, Boolean] = mutable.HashMap(
+    Input.Keys.SPACE -> false
+  )
+  var speed: Jesus = Jesus()
 
   override def onInit(): Unit = {
     setTitle("Flappy Jesus - Sebastian Cruz Go 2024")
     toDraw.add(Background())
     toDraw.add(Clouds())
-    toDraw.add(Jesus(new Vector2(150, 500)))
+    toDraw.add(Jesus())
   }
 
   override def onGraphicRender(g: GdxGraphics): Unit = {
+    println(Jesus().speed)
     g.clear()
     toDraw.forEach(element => element.draw(g))
     g.drawSchoolLogo()
+    if (keyStatus(Input.Keys.SPACE)) println("okay")
   }
+
+  // Keybord management (Jump)
+  override def onKeyDown(keycode: Int): Unit = {
+    super.onKeyDown(keycode)
+    keyStatus.put(keycode, true)
+  }
+  override def onKeyUp(keycode: Int): Unit = {
+    super.onKeyUp(keycode)
+    keyStatus.put(keycode, false)
+  }
+
 }
--- src/objects/Jesus.scala
+++ src/objects/Jesus.scala
@@ -1,23 +1,32 @@
 package ch.hevs.gdx2d.objects
+import ch.hevs.gdx2d.FlappyJesus
 import ch.hevs.gdx2d.components.bitmaps
-import ch.hevs.gdx2d.components.bitmaps.BitmapImage
+import ch.hevs.gdx2d.components.bitmaps.{BitmapImage, Spritesheet}
 import ch.hevs.gdx2d.lib.GdxGraphics
 import ch.hevs.gdx2d.lib.interfaces.DrawableObject
 import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.{Gdx, Input}
 
 
-case class Jesus(position: Vector2) extends DrawableObject {
-  private val img: BitmapImage = new bitmaps.BitmapImage("src/res/jesus.png")
+import scala.collection.mutable.HashMap
+import scala.collection.mutable
+
+
+case class Jesus() extends DrawableObject {
+  var position: Vector2 = new Vector2(150, 500)
+  private val img: Spritesheet = new Spritesheet("src/res/jesus.png", 32, 32)
   private var posy: Float = position.y
   private var posx: Float = position.x
-  private var speed: Float = 0
-
+  var speed: Float = 0
 
   override def draw(g: GdxGraphics): Unit = {
+    // Gravity
     speed += 0.5f
-    println(posy)
+
+    if(speed > 10) speed = 10
     if(posy > 100) posy -= speed
 
+    g.draw
     g.drawTransformedPicture(posx, posy, 0, 0.3f, img)
 
   }
